f1282953d0566531aff9da155bc2eb6108ddb616
Abhilash <chabhilashraju@gmail.com>
2025-07-11 07:10:04 -0500

    initialized create tables

--- database/dbmodel.py
+++ database/dbmodel.py
@@ -1,6 +1,7 @@
 # models.py
 from datetime import datetime, timezone
 from flask_sqlalchemy import SQLAlchemy
+from app import db
 
 db = SQLAlchemy()
 
@@ -12,9 +13,9 @@ class GapUpResult(db.Model):
     result_json = db.Column(db.Text)
     created_at = db.Column(db.DateTime, default=lambda: datetime.now(timezone.utc))
 
-    # To create tables (run once in a Python shell):
-    #     # >>> from app import db
-    #     # >>> db.create_all()
+    #To create tables (run once in a Python shell):
+        
+    db.create_all()
 
     def __repr__(self):
         return f"<GapUpResult {self.ticker} at {self.created_at.isoformat()}>"
